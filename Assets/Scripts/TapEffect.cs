a555cc35eddd244f6555093ce1a352204693dd51
Toshiki Hata <t.hata@oratta.net>
2017-12-29 17:15:29 +0900

    タップ時に簡単なパーティクルが表示されるようにした

---
+++ Assets/Scripts/TapEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TapEffect : MonoBehaviour
+{
+    [SerializeField]　ParticleSystem tapEffect;
+    [SerializeField]　Camera _camera;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            // z方向に値を足して、カメラより少し奥に描画する。
+            var pos = _camera.ScreenToWorldPoint(Input.mousePosition + _camera.transform.forward * 10);
+            tapEffect.transform.position = pos;
+            tapEffect.Play();
+        }
+    }
+}
